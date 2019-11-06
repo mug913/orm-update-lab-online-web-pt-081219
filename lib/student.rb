cc6ec0be693bdc183de76b1c3de2b479ddb9b4b5
Brad Alexander <brad.alexander913@gmail.com>
2019-11-06 00:47:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ require_relative "../config/environment.rb"
 class Student
 
   def initialize(name, grade, id=nil)
-    @id = id
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
